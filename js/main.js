d65fedb6a7c606e73ff5ccef44f206fa598c36dd
Franek Okrzesik <franeko@gmail.com>
2020-03-29 13:27:29 +0200

    Add sitemap in footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $(".contact-btn").click(function(event){
+    $(".show-contact-btn").click(function(event){
         event.preventDefault()
         $('#contactModal').modal('show')
     })
